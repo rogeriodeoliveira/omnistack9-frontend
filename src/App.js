579718baa9a11c2c7cfd1ff96903ca46cb8447d7
rogeriodeoliveira <blogtecnologiacominformacao@gmail.com>
2019-10-02 11:22:44 -0300

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState }  from 'react';
+import api from './services/api';
 import './App.css';
 
+import logo from './assets/logo.svg'
+
 function App() {
+  const [email, setEmail] = useState('');
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    
+    const response = await api.post('/sessions', { email });
+
+    console.log(response)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={logo} alt="Aricnc"/>
+      <div className="content">
+        <p>Orefere Spots para programadores e encontre talentos para sua empresa</p>
+        <form onSubmit={handleSubmit} >
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+          id="email" 
+          type="email" 
+          placeholder="Seu melhor E-mail" 
+          value={email}
+          onChange= {event => setEmail(event.target.value)}
+          />
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
